d4cbe2e08a7f57afd6f235fd32368e44b113cf6c
Sylvio Mafort <sylviomafort@gmail.com>
2024-11-11 16:03:07 +0900

    Create Practice2_3.java

---
+++ Practice2_3.java
@@ -0,0 +1,32 @@
+public class Practice2_3 {
+	public static void main(String[] args) {
+//		a = a + 5;
+		double a = 0;
+		a += 5;
+		System.out.println(a);
+//		b = b - 6;
+		double b = 0;
+		b -= 6;
+		System.out.println(b);
+//		c = c * a;
+		int c = 0;
+		c *= a;
+		System.out.println(c);
+//		d = d/3;
+		int d = 1;
+		d /= 3;
+		System.out.println(d);
+//		e = e % 2;
+		int e = 1;
+		e %= 2;
+		System.out.println(e);
+//		f = f + 1;
+		double f = 3;
+		f++;
+		System.out.println(f);
+//		g = g - 1;
+		double g = -3;
+		g--;
+		System.out.println(g);
+	}
+}
