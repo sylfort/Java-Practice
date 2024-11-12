6dab511dd78b1c8e86a85c6d644f4e4c19ca7f46
Sylvio Mafort <sylviomafort@gmail.com>
2024-11-12 14:35:19 +0900

    Create ForExample.java

---
+++ ForExample.java
@@ -0,0 +1,8 @@
+public class ForExample {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("forループ内の処理です。");
+		}
+		System.out.println("ループ処理を終わりました。");
+	}
+}
