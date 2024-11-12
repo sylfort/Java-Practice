4690184f55371e09fd57337ac69c1fbdab3b09dd
Sylvio Mafort <sylviomafort@gmail.com>
2024-11-12 11:59:24 +0900

    Create ElseExample.java

---
+++ ElseExample.java
@@ -0,0 +1,12 @@
+public class ElseExample {
+	public static void main(String[] args) {
+		int age = 20;
+		if (age  < 20) {
+		System.out.println("未成年ですね");
+	} else {
+		System.out.println("二十歳以上ですね");
+	}
+}
+//		String message = (age < 20) ? "未成年ですね" : "二十歳以上ですね"; 
+//		System.out.println(message);
+}
