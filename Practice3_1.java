593db2f6681161a5289e40837a7e3e49e4700d2e
Sylvio Mafort <sylviomafort@gmail.com>
2024-11-13 09:35:34 +0900

    Create Practice3_1.java

---
+++ Practice3_1.java
@@ -0,0 +1,12 @@
+public class Practice3_1 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 3;
+		int c = 4;
+		if(a==b) System.out.println("a==b");
+		if(a!=b) System.out.println("a!=b");
+		if(b<c) System.out.println("b<c");
+		if(a<=b) System.out.println("a<=b");
+		if(c>=b) System.out.println("c>=b");
+	}
+}
