da8ea4637b95bc09ff5e4fd8b567389c3b9a67a2
Sylvio Mafort <sylviomafort@gmail.com>
2024-11-28 10:17:34 +0900

    Create ArrayListExample.java

---
+++ ArrayListExample.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		ArrayList<String> months = new ArrayList<String>();
+		
+		months.add("January");
+		months.add("February");
+		months.add("March");
+		
+		System.out.println("要素数　" + months.size());
+		for (int i = 0; i < months.size(); i++) {
+			System.out.println(months.get(i));
+		}
+		
+		months.remove(1);
+		System.out.println("要素数　" + months.size());
+		for (int i = 0; i < months.size(); i++) {
+			System.out.println(months.get(i));
+		}
+	}
+}
