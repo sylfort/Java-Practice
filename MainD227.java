876c5852e1adfaa8fdb38e7e48d8849f76a382cb
Sylvio Mafort <sylviomafort@gmail.com>
2024-12-17 15:23:02 +0900

    Create MainD227.java

---
+++ MainD227.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MainD227 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        
+        System.out.println(minus(Integer.valueOf(line)));
+    }
+    
+    public static int minus(int num){
+        return num - 120;
+    }
+}
