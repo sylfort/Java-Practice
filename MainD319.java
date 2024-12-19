56e2a0c09d978c6a766fa26f9006605c2de3e685
Sylvio Mafort <sylviomafort@gmail.com>
2024-12-19 10:47:12 +0900

    Create MainD319.java

---
+++ MainD319.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MainD319 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String[] arr = line.split("");
+        System.out.println(arr[0] + arr[1] + arr[2]);
+    }
+}
