2a52c1abd8d0eff983ad68239fdfaa534f3d1843
Sylvio Mafort <sylviomafort@gmail.com>
2024-12-23 15:22:37 +0900

    Create MainD174.java

---
+++ MainD174.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MainD174 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        System.out.println(line * 1500);
+    }
+}
