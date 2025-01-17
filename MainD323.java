6d1533b777fcc82438f02e9856166419257afcd8
Sylvio Mafort <sylviomafort@gmail.com>
2025-01-17 15:46:41 +0900

    Create MainD323.java

---
+++ MainD323.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MainD323 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        System.out.println(line * 3);
+    }
+}
